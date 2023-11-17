91508b889941c2cfbb4710135841e6514b3b5712
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2023-11-17 08:57:16 +0800

    make folder group

---
+++ CafesPlatform/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  CafesPlatform
+//
+//  Created by Jiaxin Pu on 2023/11/17.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "哈哈"
+        
+        self.view.backgroundColor = .white
+    }
+    
+
+}
